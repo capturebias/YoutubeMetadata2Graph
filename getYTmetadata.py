09929a05e472e4d2a2251bc3d9bba7ddc2da8575
Xander Wilcke <w.x.wilcke@vu.nl>
2019-06-03 17:19:02 +0200

    first version

--- getYTmetadata.py
+++ getYTmetadata.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-from sys import stdin
+from datetime import datetime
+from json import dump
+from requests import exceptions
+from sys import stderr, stdin, stdout
 
 from googleapiclient.discovery import build
 
@@ -17,7 +20,7 @@ def video(video_identifiers):
 def read_developer_key(keyfile):
     key = None
     with open(keyfile, "r") as kf:
-        key = kf.read.split()
+        key = kf.read().split()[0]
 
     return key
 
@@ -25,41 +28,84 @@ def build_service_object(api_service_name, api_version, developer_key):
     return build(api_service_name, api_version, developerKey=developer_key)
 
 def request_channel_data(service, channel_id):
+    # https://developers.google.com/youtube/v3/docs/channels
+    #
+    # - The snippet object contains basic details about the channel, such as its
+    #   title, description, and thumbnail images.
+    # - The contentDetails object encapsulates information about the channel's
+    #   content.
+    # - The statistics object encapsulates statistics for the channel.
+    # - The topicDetails object encapsulates information about topics associated
+    #   with the channel.
+    # - The brandingSettings object encapsulates information about the branding
+    #   of the channel.
     request = service.channels().list(
-        part="snippet,contentDetails,statistics",
+        part="snippet,contentDetails,statistics,topicDetails,brandingSettings",
         id=channel_id
     )
-    response = request.execute()
+
+    return request_data(request)
 
 def request_video_data(service, video_id):
+    # https://developers.google.com/youtube/v3/docs/videos
+    #
+    # - The snippet object contains basic details about the video, such as its
+    #   title, description, and category.
+    # - The contentDetails object contains information about the video content,
+    #   including the length of the video and an indication of whether captions
+    #   are available for the video.
+    # - The statistics object contains statistics about the video.
+    # - The topicDetails object encapsulates information about topics
+    #   associated with the video.
     request = service.videos().list(
-        part="snippet,contentDetails,statistics",
+        part="snippet,contentDetails,statistics,topicDetails",
         id=video_id
     )
-    response = request.execute()
+
+    return request_data(request)
+
+def request_data(request):
+    try:
+        response = request.execute()
+    except exceptions.RequestException as e:
+        stderr.write("Request Error: %s\n" % (e))
+        return (dict(), False)
+
+    return (response['items'][0], True) # strip request meta data
 
 def main():
     developer_key = read_developer_key(DEVELOPER_KEY_FILE)
     service = build_service_object(API_SERVICE_NAME, API_VERSION, developer_key)
 
-    channels_visited = set()
+    data = dict()
     for video_id in video(stdin):
-        channel_id, video_data = request_video_data(service, video_id)
+        video_data, success = request_video_data(service, video_id)
+        if not success:
+            stderr.write("Failed retrieving video %s\n" % (video_id))
+            continue
 
-        # add video_data to tsv
-        if channel_id not in channels_visited:
-            channel_data = request_channel_data(service, channel_id)
+        video_data['retrieved_on'] = datetime.today().strftime("%Y-%m-%dT%H:%M:%S")
 
-            # add channel_data to tsv
-            channels_visited.add(channel_id)
+        # extract channel ID
+        channel_id = video_data['snippet']['channelId']
+        if channel_id not in data.keys():
+            channel_data, success = request_channel_data(service, channel_id)
+            if not success:
+                stderr.write("Failed retrieving channel %s\n" % (channel_id))
+            else:
+                channel_data['retrieved_on'] = datetime.today().strftime("%Y-%m-%dT%H:%M:%S")
 
-        # add other types of linked information?
+            channel_data['videos'] = list()
 
-        # deal with errors
-        # add retrieval dateTime
+            # add new channel
+            data[channel_id] = channel_data
 
-    # tsv close
-    # print stats
+        # add new video to channel
+        data[channel_id]['videos'].append(video_data)
+
+    return data
 
 if __name__ == "__main__":
-    main()
+    data = main()
+
+    dump(data, stdout, indent=4)
